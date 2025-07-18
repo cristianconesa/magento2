dea3bede2cb8989dbcef545bd6014b71e1010745
Cristian Conesa <cristiancitoconesa@gmail.com>
2025-07-17 21:23:35 -0300

    adds

--- pub/static/frontend/Magento/luma/es_AR/Conesa_ScheduleDelivery/js/view/shipping-method-list.js
+++ pub/static/frontend/Magento/luma/es_AR/Conesa_ScheduleDelivery/js/view/shipping-method-list.js
@@ -1,12 +1,36 @@
 define([
-    'uiComponent'
-], function (Component) {
+    'uiComponent',
+    'ko',
+    'Magento_Checkout/js/model/shipping-service',
+    'Magento_Checkout/js/model/quote'
+], function (Component, ko, shippingService, quote) {
     'use strict';
 
     return Component.extend({
+        defaults: {
+            template: 'Conesa_ScheduleDelivery/shipping-method-list'
+        },
+
+        // Observable to show/hide button
+        isButtonVisible: ko.observable(false),
+
         initialize: function () {
             this._super();
-            console.log('Conesa_ScheduleDelivery: shipping-method-list JS loaded');
+
+            // Observe shipping methods and toggle button
+            this.shippingMethods = shippingService.getShippingRates();
+            this.isButtonVisible = ko.computed(function () {
+                return this.shippingMethods() && this.shippingMethods().length > 0;
+            }, this);
+
+            return this;
+        },
+
+        // Handler for button click
+        scheduleDelivery: function () {
+            // Ejemplo: mostrar un alert, puedes cambiar por modal, action, etc.
+            alert('¡Funcionalidad: Programar Entrega!');
+            // Aquí puedes disparar lógica adicional, abrir modal, etc.
         }
     });
 });
